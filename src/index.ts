9d6cfc62dbc351b9bc3b051026e74d6758b1c724
old-horizon <qsd8650@hotmail.co.jp>
2020-12-15 21:40:12 +0900

    output errors to console

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,32 @@ import wol from 'wakeonlan';
 const region = process.env['REGION']!!;
 const queueUrl = process.env['SQS_QUEUE_URL']!!;
 
-AWS.config.update({region});
+AWS.config.update({ region });
 const sqs = new AWS.SQS();
 
 console.log('Waiting wake messages...');
 
 (async () => {
     while (true) {
-        const data = await sqs.receiveMessage({
-            QueueUrl: queueUrl,
-            MaxNumberOfMessages: 10,
-            WaitTimeSeconds: 20
-        }).promise();
-
-        for (const message of data.Messages || []) {
-            const wakeMessage = JSON.parse(message.Body!!) as WakeMessage;
-            await wol(wakeMessage.macAddress);
-            console.log(`Magic packet has sent: ${wakeMessage.macAddress}`);
-
-            await sqs.deleteMessage({
+        try {
+            const data = await sqs.receiveMessage({
                 QueueUrl: queueUrl,
-                ReceiptHandle: message.ReceiptHandle!!
+                MaxNumberOfMessages: 10,
+                WaitTimeSeconds: 20
             }).promise();
+
+            for (const message of data.Messages || []) {
+                const wakeMessage = JSON.parse(message.Body!!) as WakeMessage;
+                await wol(wakeMessage.macAddress);
+                console.log(`Magic packet has sent: ${wakeMessage.macAddress}`);
+
+                await sqs.deleteMessage({
+                    QueueUrl: queueUrl,
+                    ReceiptHandle: message.ReceiptHandle!!
+                }).promise();
+            }
+        } catch (e) {
+            console.error(e);
         }
     }
 })();
